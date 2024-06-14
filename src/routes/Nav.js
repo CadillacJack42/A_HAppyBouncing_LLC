f5b5b204baff225ecc4ba2f9529fe1284f49beef
James Keicher <jamesdkeicher@gmail.com>
2024-06-14 12:17:07 -0700

    connected to supabase

--- src/routes/Nav.js
+++ src/routes/Nav.js
@@ -5,8 +5,11 @@ export const Nav = () => {
     <div id="app-container">
       <h1 id="company-name">A Happy Bouncing LLC</h1>
       <nav>
-        <a href="/">Products</a> |<a href="/account">Account</a> |
-        <a href="/about">About</a>
+        {/* Products page is HOME component */}
+        <a href="/">Products</a> {" | "}
+        <a href="/account">Account</a> {" | "}
+        <a href="/about">About</a> {" | "}
+        <a href="/auth">Login</a>
       </nav>
       <Outlet />
     </div>
